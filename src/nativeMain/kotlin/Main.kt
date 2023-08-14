d426a438775795af82b87a56e1ec1f469dd22594
Alex <mr.alex.dmitriev@gmail.com>
2023-08-14 22:16:02 +0300

    Add NCurses sample windows

--- src/nativeMain/kotlin/Main.kt
+++ src/nativeMain/kotlin/Main.kt
@@ -2,11 +2,11 @@
 
 import com.kgit2.process.Command
 import com.kgit2.process.Stdio
-import kotlinx.cinterop.ExperimentalForeignApi
-import kotlin.text.Regex.Companion.escape
 import kotlinx.cinterop.CPointer
+import kotlinx.cinterop.ExperimentalForeignApi
 import kotlinx.cinterop.memScoped
 import ncurses.*
+import platform.posix.*
 
 val r2 = """^([A-Z])/(.+?)\( *(\d+)\): (.*?)$""".toRegex()
 
@@ -58,6 +58,107 @@ fun show() {
 
 @OptIn(ExperimentalForeignApi::class)
 fun main() = memScoped {
+    setlocale(LC_CTYPE, "")
+    //setlocale(LC_ALL, "en_US.UTF-8");
+    initscr();
+    savetty();
+    noecho();//disable auto-echoing
+    cbreak();//making getch() work without a buffer I.E. raw characters
+    //keypad(stdscr, true);//allows use of special keys, namely the arrow keys
+    //clear();    // empty the screen
+    timeout(0); // reads do not block
+
+    val fp = newpad(50, 120)
+    scrollok(fp, true)
+
+    (1..200).forEach {
+        //wprintw(fp, "%d - line ------------------------------------------------------------------------  \n", it);
+        waddstr(fp,"*** PROCESS 01 ***")
+    }
+
+    prefresh(fp,0, 0, 0,0, 20,120);
+
+    sleep(1000U)
+
+    getch()
+    //exit(0)
+    while (true) {
+        val ch = getwchar()
+
+        wscrl(fp, -1)
+    }
+
+    //val p = newpad(80,COLS);
+    //keypad(p, true);
+
+    //getmaxyx(stdscr, height, width);
+    val p = newwin(50 - 2, 150 - 2, 1, 1);
+
+    scrollok(p,true);
+
+   /* val child1 = Command("adb")
+        .args("logcat", "-v", "brief")
+        .stdout(Stdio.Pipe)
+        .spawn()*/
+
+    //val stdoutReader1: com.kgit2.io.Reader? = child1.getChildStdout()
+
+    (1..100).forEach {
+        //waddstr(p, "aaa$it")
+        val sm = "\u001b[263a]"
+        try {
+            //val line = stdoutReader1!!.readLine()!!// ?: exit(127)
+            //sleep(1U)
+            wprintw(p, "\u2588")
+            wprintw(p, "\u263a")
+            wprintw(p, "%d - $sm line  \n", it);
+        } catch (e: Exception) {
+            wprintw(p, "----------------------------> ${e.message}")
+
+            wprintw(p, "----------------------------> ${e.cause}")
+            sleep(100U)
+        }
+        wrefresh(p)
+    }
+
+
+    //prefresh(p,0, 0, 0,0, LINES-1,COLS-1);
+    //prefresh(pad, mypadpos, 0, 0, 0, mrow, mcol);
+
+
+    while (true) {
+        val ch = getwchar()
+
+        //wget_wch()
+        val c = Char(ch)
+
+        wscrl(p, -1)
+
+        when (ch) {
+            KEY_UP -> wprintw(p,"Up\n")
+            KEY_LEFT -> wprintw(p,"Left\n")
+            KEY_RIGHT -> wprintw(p,"Right\n")
+            KEY_BACKSPACE -> wprintw(p,"Backspace\n")
+            KEY_ENTER -> {
+                wprintw(p,"You pressed Enter\n")
+                //printw("%u\n", ch)
+            }
+
+            else ->{}// printw("%u\n", ch)
+        }
+
+        //wprintw(p, "$KEY_UP $ch $c\n")
+        //wprintw(p, "Auftrag \tName \t\t\tZeit\n");
+        wrefresh(p)
+        //prefresh(p, 0, 0, 0, 0, LINES - 1, COLS -1)
+    }
+
+    //getmaxyx(stdscr, mrow, mcol);
+    getch()
+    //endwin();
+    //exit(0)
+
+    //getmaxx()
     //show()
     initscr();
     val new = newwin(80 - 2, 80 - 2, 1, 1);
