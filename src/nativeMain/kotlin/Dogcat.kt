37252419afa2f3b6ae12c8fe1f3a94aec411afb6
Alex <mr.alex.dmitriev@gmail.com>
2023-09-08 08:38:15 +0300

    Re-create 'sharedLines' on each logcat clearing

--- src/nativeMain/kotlin/Dogcat.kt
+++ src/nativeMain/kotlin/Dogcat.kt
@@ -5,8 +5,8 @@ import kotlinx.coroutines.flow.*
 @OptIn(ExperimentalCoroutinesApi::class)
 class Dogcat(
     private val logSource: LogSource,
-    dispatcherCpu: CoroutineDispatcher = Dispatchers.Default,
-    dispatcherIo: CoroutineDispatcher = Dispatchers.IO,
+    private val dispatcherCpu: CoroutineDispatcher = Dispatchers.Default,
+    private val dispatcherIo: CoroutineDispatcher = Dispatchers.IO,
     private val lineParser: LogLineParser = LogcatBriefParser()
 )  {
     val handler = CoroutineExceptionHandler { _, t -> println("999999 ${t.message}\r") }
@@ -20,44 +20,43 @@ class Dogcat(
 
     private val logLevels = mutableSetOf<String>("V", "D", "I", "W", "E") //+.WTF()?
 
-    //re-create each time clearing occurs
-    private val sharedLines = logSource // deal with malformed UTF-8 'expected one more byte'
-        .lines()
-        //.onEach { println("----- >> each $it\r") }
-        .retry(3) { e ->
-            println("retrying...\r")
-            val shallRetry = e is RuntimeException
-            if (shallRetry) delay(100)
-            println("retrying... $shallRetry\r")
-            shallRetry
-        }
-        .onCompletion { cause -> if (cause == null) emit("INPUT HAS EXITED") else println("EXIT COMPLETE $cause\r")}
-        .flowOn(dispatcherIo)
-        .onEmpty { println("empty\r") }
-        .onStart { println("start logcat lines\r") }
-        .shareIn(
-            scope,
-            SharingStarted.Lazily,
-            Config.LogLinesBufferCount,
-        )
-        .onSubscription { println("subscr to shared lines\r") }
-        .onCompletion { println("shared compl!\r") }
-
-    private fun filterLines() = filterLine
-        .flatMapLatest { filter ->
-            sharedLines
-                .filter { it.contains(filter) }
-        }
-        .map { lineParser.parse(it) }
-        .filter {
-            if (it is Parsed) {
-                logLevels.contains(it.level)
-            } else {
-                true
+    private fun filterLines(): Flow<LogLine> {
+        val sharedLines = logSource // deal with malformed UTF-8 'expected one more byte'
+            .lines()
+            .retry(3) { e ->
+                val shallRetry = e is RuntimeException
+                if (shallRetry) delay(100)
+                println("retrying... $shallRetry\r")
+                shallRetry
             }
-        }
-        //.flowOn()
-        .onCompletion { println("outer compl\r") }
+            .takeUntil(stopSubject)
+            .onCompletion { cause -> if (cause == null) emit("INPUT HAS EXITED") else println("EXIT COMPLETE $cause\r") }
+            .flowOn(dispatcherIo)
+            .onStart { println("start logcat lines\r") }
+            .shareIn(
+                scope,
+                SharingStarted.Lazily,
+                Config.LogLinesBufferCount,
+            )
+            .onSubscription { println("subscr to shared lines\r") }
+            .onCompletion { println("shared compl!\r") }
+
+
+        return filterLine
+            .flatMapLatest { filter ->
+                sharedLines.filter { it.contains(filter) }
+            }
+            .map { lineParser.parse(it) }
+            .filter {
+                if (it is Parsed) {
+                    logLevels.contains(it.level)
+                } else {
+                    true
+                }
+            }
+            //.flowOn()
+            .onCompletion { println("outer compl\r") }
+    }
 
     suspend operator fun invoke(cmd: LogcatCommands) {
         when (cmd) {
@@ -76,7 +75,6 @@ class Dogcat(
 
                 scope.ensureActive()
                 scope.cancel()
-                println("cancelled scope ${scope.coroutineContext.isActive}\r")
             }
         }
     }
@@ -105,8 +103,6 @@ class Dogcat(
 
         stopSubject.emit(Unit)
         println("called stop subject..\r")
-
-        //sharedLines.rese
 
         logSource.clear()
         val ci = LogcatState.InputCleared
