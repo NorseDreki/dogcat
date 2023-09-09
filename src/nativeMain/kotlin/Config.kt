f6eca1be13a10d9adda110fe90c646bda32459b5
Alex <mr.alex.dmitriev@gmail.com>
2023-09-09 11:18:17 +0300

    Add more config items

--- src/nativeMain/kotlin/Config.kt
+++ src/nativeMain/kotlin/Config.kt
@@ -1,4 +1,17 @@
 object Config {
 
     const val LogLinesBufferCount = 32767
+
+    const val tagWidth = 40
+
+    const val alwaysDisplayTagName = false
+
+    const val colorGc = false
+
+    //color strict mode
+    //color swallowed exceptions
+    //color fatal exception
+    // predefined colors for known tags
+    //backtrace after a native crash
+
 }
