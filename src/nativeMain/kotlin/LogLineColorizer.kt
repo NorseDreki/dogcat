466ca420290cbd22f5371811b066c0b4ef5d9417
Alex <mr.alex.dmitriev@gmail.com>
2023-09-09 09:34:24 +0300

    Remove excess output separators

--- src/nativeMain/kotlin/LogLineColorizer.kt
+++ src/nativeMain/kotlin/LogLineColorizer.kt
@@ -8,7 +8,7 @@ class LogLineColorizer {
         fp: CPointer<WINDOW>?,
         it: IndexedValue<LogLine>,
     ) {
-        waddstr(fp, "${it.index} ")
+        //waddstr(fp, "${it.index} ")
         val logLine = it.value
 
         if (logLine is Original) {
@@ -16,41 +16,41 @@ class LogLineColorizer {
 
         } else if (logLine is Parsed) {
 
-            waddstr(fp, "${logLine.tag} ||")
+            waddstr(fp, "${logLine.tag} ")
             /*wattron(fp, COLOR_PAIR(1));
                     waddstr(fp, "${it.value.tag} ||")
                     //wprintw(fp, it.value)
                     wattroff(fp, COLOR_PAIR(1))*/
 
             //wattron(fp, COLOR_PAIR(2));
-            waddstr(fp, "${logLine.owner} ||")
+            //waddstr(fp, "${logLine.owner} ||")
             //wattroff(fp, COLOR_PAIR(2));
 
             when (logLine.level) {
                 "W" -> {
                     wattron(fp, COLOR_PAIR(3));
-                    waddstr(fp, "${logLine.level} ||")
-                    waddstr(fp, "${logLine.message} ||\n")
+                    waddstr(fp, "[${logLine.level}] ")
+                    waddstr(fp, "${logLine.message}\n")
                     wattroff(fp, COLOR_PAIR(3));
                 }
 
                 "E" -> {
                     wattron(fp, COLOR_PAIR(1));
-                    waddstr(fp, "${logLine.level} ||")
-                    waddstr(fp, "${logLine.message} ||\n")
+                    waddstr(fp, "[${logLine.level}] ")
+                    waddstr(fp, "${logLine.message}\n")
                     wattroff(fp, COLOR_PAIR(1));
                 }
 
                 "I" -> {
                     //wattron(fp, COLOR_PAIR(4));
-                    waddstr(fp, "${logLine.level} ||")
-                    waddstr(fp, "${logLine.message} ||\n")
+                    waddstr(fp, "[${logLine.level}] ")
+                    waddstr(fp, "${logLine.message}\n")
                     //wattroff(fp, COLOR_PAIR(4));
                 }
 
                 else -> {
-                    waddstr(fp, "${logLine.level} ||")
-                    waddstr(fp, "${logLine.message} ||\n")
+                    waddstr(fp, "[${logLine.level}] ")
+                    waddstr(fp, "${logLine.message}\n")
                 }
             }
             //waddstr(fp, "${it.value.message} \n")
