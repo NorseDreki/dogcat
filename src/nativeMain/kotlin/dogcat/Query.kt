842fefe4e91a8477a5a1e4e6748ce1622f9e822e
Alex <mr.alex.dmitriev@gmail.com>
2023-09-24 17:05:51 +0300

    Use immutable approach for filters state

--- src/nativeMain/kotlin/dogcat/Query.kt
+++ src/nativeMain/kotlin/dogcat/Query.kt
@@ -1,8 +1,12 @@
 package dogcat
 
+import dogcat.LogFilter.*
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.yield
 import platform.Logger
 import kotlin.reflect.KClass
 
@@ -13,26 +17,28 @@ interface Query {
 
 class InternalQuery : Query {
 
-    private val appliedFiltersState = MutableStateFlow<AppliedFilters>(mutableMapOf())
+    private val defaultFilters: AppliedFilters =
+        mutableMapOf(
+            Substring::class to (Substring("") to true),
+            MinLogLevel::class to (MinLogLevel("V") to true)
+        )
+
+    private val appliedFiltersState = MutableStateFlow(defaultFilters)
     override val appliedFilters = appliedFiltersState.asStateFlow()
 
-    private val af: AppliedFilters = mutableMapOf(LogFilter.Substring::class to (LogFilter.Substring("") to true))
-
     suspend fun upsertFilter(filter: LogFilter, enable: Boolean = true) {
-        af[filter::class] = filter to enable
-        appliedFiltersState.emit(af)
+        val next = appliedFiltersState.value + (filter::class to (filter to true))
+        appliedFiltersState.emit(next)
 
-
-        Logger.d("sub ${appliedFiltersState.subscriptionCount.value} $af")
-
-        Logger.d("zzzz ${appliedFiltersState.value}")
+        Logger.d("Upsert filter: ${appliedFiltersState.subscriptionCount.value} $next")
     }
 
     suspend fun removeFilter(filter: KClass<out LogFilter>) {
-        af.remove(filter)
-        appliedFiltersState.emit(af)
-        Logger.d("After removing: $af")
+        val a = appliedFiltersState.value - filter
+        appliedFiltersState.emit(a)
+
+        Logger.d("After removing: ${appliedFiltersState.value}")
     }
 }
 
-typealias AppliedFilters = MutableMap<KClass<out LogFilter>, Pair<LogFilter, Boolean>>
+typealias AppliedFilters = Map<KClass<out LogFilter>, Pair<LogFilter, Boolean>>
