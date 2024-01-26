8a5b44ded1939d051344a5e59da515a77edc002b
Alex <mr.alex.dmitriev@gmail.com>
2024-01-26 10:48:42 +0300

    Add draft for color map

--- src/nativeMain/kotlin/ui/AppView.kt
+++ src/nativeMain/kotlin/ui/AppView.kt
@@ -10,64 +10,42 @@ import platform.posix.setlocale
 @OptIn(ExperimentalForeignApi::class)
 class AppView {
 
-    suspend fun start() {
+    fun start() {
         setlocale(LC_ALL, "en_US.UTF-8") // should be before initscr()
         initscr()
-
-        //raw();
 
         //The keypad function enables the reading of function keys like arrow keys, Home, End, and so on.
         keypad(stdscr, true);
         noecho();
 
-
         //intrflush(stdscr, false)
-        //savetty()
-        //noecho()
-        //savetty()
 
         //nl()
         //Use the ncurses functions for output. My guess is that initscr changes terminal settings such that \n only performs a line feed, not a carriage return. –
         //melpomene
 
-        //nodelay(stdscr, true) //The nodelay option causes getch to be a non-blocking call. If no input is ready, getch returns ERR. If disabled (bf is  FALSE),  getch waits until a key is pressed
-        //cbreak(); //Disable line buffering
+        nodelay(stdscr, true) //The nodelay option causes getch to be a non-blocking call. If no input is ready, getch returns ERR. If disabled (bf is  FALSE),  getch waits until a key is pressed
         //cbreak() //making getch() work without a buffer I.E. raw characters
-        //keypad(stdscr, true) //allows use of special keys, namely the arrow keys
-        //clear()
-
-        //whline(topBar, '_',80); //draw line for bottom
 
         if (!has_colors()) {
             endwin()
             printf("Your terminal does not support color\n")
             exit(1)
         }
+
         use_default_colors()
         start_color()
         init_pair(1, COLOR_RED.toShort(), -1)
         init_pair(2, COLOR_GREEN.toShort(), -1)//COLOR_BLACK.toShort())
         init_pair(3, COLOR_YELLOW.toShort(), -1)
-        init_pair(4, COLOR_CYAN.toShort(), COLOR_BLACK.toShort())
+        //init_pair(4, COLOR_CYAN.toShort(), COLOR_BLACK.toShort())
 
         init_pair(11, COLOR_BLACK.toShort(), COLOR_RED.toShort())
         init_pair(12, COLOR_BLACK.toShort(), COLOR_WHITE.toShort())
         init_pair(6, COLOR_BLACK.toShort(), COLOR_YELLOW.toShort())
-
-
-        /*raw();
-        noraw()*/
-
-        //fflush(stdout); // added
-        //hideCursor()
     }
 
     fun stop() {
         endwin()
     }
-
-    private fun hideCursor() {
-        curs_set(0)
-    }
-
 }
--- src/nativeMain/kotlin/ui/logLines/TagExtensions.kt
+++ src/nativeMain/kotlin/ui/logLines/TagExtensions.kt
@@ -8,6 +8,8 @@ package ui.logLines
 import AppConfig.DEFAULT_TAG_WIDTH
 import kotlinx.cinterop.ExperimentalForeignApi
 import ncurses.*
+import ui.ColorMap.allocateColor
+import ui.ColorMap.ccpp
 
 internal fun LogLinesView.printTag(tag: String) {
     val c = allocateColor(tag)
@@ -16,46 +18,6 @@ internal fun LogLinesView.printTag(tag: String) {
     waddstr(pad, tag.massage())
     wattroff(pad, COLOR_PAIR(ccpp[c]!!))
 }
-
-private fun allocateColor(tag: String): Int {
-    if (!KNOWN_TAGS.containsKey(tag)) {
-        KNOWN_TAGS[tag] = LAST_USED[0]
-    }
-    val color = KNOWN_TAGS[tag]!!
-
-    if (LAST_USED.contains(color)) {
-        LAST_USED.remove(color)
-        LAST_USED.add(color)
-    }
-
-    return color
-}
-
-private val LAST_USED = mutableListOf(
-    /*COLOR_RED,*/
-    COLOR_GREEN,
-    /*COLOR_YELLOW,*/
-    COLOR_BLUE,
-    COLOR_MAGENTA,
-    COLOR_CYAN
-)
-
-private val KNOWN_TAGS = mutableMapOf(
-    "dalvikvm" to COLOR_WHITE,
-    "Process" to COLOR_WHITE,
-    "ActivityManager" to COLOR_WHITE,
-    "ActivityThread" to COLOR_WHITE,
-    "AndroidRuntime" to COLOR_CYAN,
-    "jdwp" to COLOR_WHITE,
-    "StrictMode" to COLOR_WHITE,
-    "DEBUG" to COLOR_YELLOW,
-)
-
-private val ccpp = (LAST_USED + KNOWN_TAGS.values)
-    .associateWith {
-        init_pair((100 + it).toShort(), it.toShort(), -1)
-        (100 + it)
-    }
 
 private fun String.massage() = //prokrust
     if (length > DEFAULT_TAG_WIDTH) {
