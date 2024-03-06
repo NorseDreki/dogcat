88c93b9b17e8d6831a5231c53304af772ede3cfc
Alex <mr.alex.dmitriev@gmail.com>
2024-03-06 16:14:36 +0300

    Finalize keymap

--- src/nativeMain/kotlin/com/norsedreki/dogcat/app/Keymap.kt
+++ src/nativeMain/kotlin/com/norsedreki/dogcat/app/Keymap.kt
@@ -8,29 +8,27 @@ object Keymap {
 
     @OptIn(ExperimentalForeignApi::class)
     val bindings = mapOf(
-        'p'.code to AUTOSCROLL,
+        'r'.code to AUTOSCROLL,
         'q'.code to QUIT,
 
         'f'.code to INPUT_FILTER_BY_SUBSTRING,
 
         'a'.code to HOME,
         KEY_HOME to HOME,
-        'z'.code to END,
+        'd'.code to END,
         KEY_END to END,
-        'w'.code to LINE_UP,
         KEY_UP to LINE_UP,
-        's'.code to LINE_DOWN,
         KEY_DOWN to LINE_DOWN,
-        'd'.code to PAGE_DOWN,
+        's'.code to PAGE_DOWN,
         KEY_NPAGE to PAGE_DOWN,
-        'e'.code to PAGE_UP,
+        'w'.code to PAGE_UP,
         KEY_PPAGE to PAGE_UP,
 
         'c'.code to CLEAR_LOGS,
 
-        '6'.code to TOGGLE_FILTER_BY_PACKAGE,
-        '7'.code to RESET_FILTER_BY_SUBSTRING,
-        '8'.code to RESET_FILTER_BY_MIN_LOG_LEVEL,
+        't'.code to TOGGLE_FILTER_BY_PACKAGE,
+        //'7'.code to RESET_FILTER_BY_SUBSTRING,
+        //'8'.code to RESET_FILTER_BY_MIN_LOG_LEVEL,
 
         '1'.code to MIN_LOG_LEVEL_V,
         '2'.code to MIN_LOG_LEVEL_D,
