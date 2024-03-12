47f2f608402561153a41317028e5704eb61884d9
Alex <mr.alex.dmitriev@gmail.com>
2024-03-12 11:29:52 +0300

    UInt / Ulong issue

--- src/nativeMain/kotlin/com/norsedreki/dogcat/app/ui/Input.kt
+++ src/nativeMain/kotlin/com/norsedreki/dogcat/app/ui/Input.kt
@@ -132,7 +132,8 @@ class DefaultInput(
 
                 if (char in ' '..'~') {
                     inputBuffer.insert(cursorPosition - inputX, char)
-                    //mvaddch(inputY, cursorPosition, key.toUInt())
+
+                    mvaddch(inputY, cursorPosition, key.toUInt())
 
                     cursorPosition++
                 }
