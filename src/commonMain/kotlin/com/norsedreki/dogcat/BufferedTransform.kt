e92a7a1e580f42b40768de52f329b2f561dccd04
Alex Dmitriev <mr.alex.dmitriev@icloud.com>
2024-05-24 17:02:00 +0300

    Remove unused code

--- src/commonMain/kotlin/com/norsedreki/dogcat/BufferedTransform.kt
+++ src/commonMain/kotlin/com/norsedreki/dogcat/BufferedTransform.kt
@@ -5,8 +5,6 @@
 
 package com.norsedreki.dogcat
 
-import kotlin.time.Duration
-import kotlin.time.TimeSource
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
@@ -19,46 +17,11 @@ fun <T> Flow<T>.bufferedTransform(
     collect { item ->
         val willEmpty = shouldEmptyBuffer(storage, item)
         if (willEmpty) {
-            // storage.onEach { emit(it) }
             storage.clear()
         }
 
         val newItem = transformItem(storage, item)
         storage.add(newItem)
         emit(newItem)
-    }
-}
-
-fun <T> Flow<T>.debounceRepetitiveKeys(debounceTime: Duration): Flow<T> = flow {
-    var lastKey: T? = null
-    var lastEmissionTime = TimeSource.Monotonic.markNow()
-
-    collect { key ->
-        val currentTime = TimeSource.Monotonic.markNow()
-        if (
-            key != lastKey ||
-                currentTime.elapsedNow() - lastEmissionTime.elapsedNow() >= debounceTime
-        ) {
-            emit(key)
-            lastKey = key
-            lastEmissionTime = currentTime
-        }
-    }
-}
-
-fun <T> Flow<T>.windowed(time: Duration): Flow<List<T>> = flow {
-    val window = mutableListOf<T>()
-    val startTime = TimeSource.Monotonic.markNow()
-
-    collect { value ->
-        window.add(value)
-        if (TimeSource.Monotonic.markNow() - startTime >= time) {
-            emit(window.toList())
-            window.clear()
-        }
-    }
-
-    if (window.isNotEmpty()) {
-        emit(window.toList())
     }
 }
--- src/nativeMain/kotlin/com/norsedreki/dogcat/app/AppConfig.kt
+++ src/nativeMain/kotlin/com/norsedreki/dogcat/app/AppConfig.kt
@@ -32,16 +32,4 @@ object AppConfig {
     const val DEFAULT_LOCALE = "en_US.UTF-8"
 
     const val EXIT_CODE_ERROR = 1
-
-    // Make sure pattern matching works on MinGW using '\n'
-    // There is no 'line.separator' or 'Environment.NewLine'
-    // "\r?\n|\r".toRegex()
-    // To match with any Unicode linebreak sequence, we can use the linebreak matcher \R
-    // \\R
-    // [\r\n]+
-    const val LINE_SEPARATOR = "\n"
-    // If you write this in common module, it should get translated to a correct value on a
-    // respective platform.
-    // No need to use expect-actual for this.
-    // By the Kotlin native compiler when it builds native code for a respective platform
 }
